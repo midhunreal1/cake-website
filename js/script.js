f28da34b767e16318bf1118d270c8c90b43c5806
Midhun M S <89185752+midhunreal1@users.noreply.github.com>
2023-08-02 20:52:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 
+    let elem =document.getElementsByClassName('content');
+    let toggle = document.querySelector('#toggle');
+    toggle.addEventListener('click',()=>{
+        let navItems= document.querySelector('.nav-items');
+        if(navItems.style.display=="none") {
+            navItems.style.display="block"
+            
+        }else{
+            navItems.style.display="none"
+        }
+    })
+
